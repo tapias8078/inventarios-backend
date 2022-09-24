c53054a9081993c0121974f41b936ea1d8f59463
DaniellaVT <daniela.tapias@est.iudigital.edu.co>
2022-09-24 01:59:53 -0500

    Commit

--- router/inventario.js
+++ router/inventario.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const Invetario = require('../models/Inventario')
 const {validarInventario} = require('../helpers/validar-inventario')
+const Inventario = require('../models/Inventario')
 
 
 const router = Router()
@@ -88,5 +89,18 @@ router.put('/:inventarioId', async function(req,res){
          res.status(500).send('ocurrio un error')
      }
 })
+router. get ('/:inventarioId', async function (req, res) {
+    try { 
+        const inventario = await Inventario.findById (req.params.inventarioId);
+        if (!inventario) {
+            return res.status (404).send('Inventario no existe');
+        }
+        res.send(inventario);
 
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Ocurrio un error al consultar el inventario');
+
+    }
+}); 
 module.exports = router
